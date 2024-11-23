1dbcf1b7010b8fda42a5184127d83c35b5a0367d
MattyCi <mjciaravino@gmail.com>
2024-11-22 23:18:08 -0500

    redis sentinel testing

--- aws-basic-tutorial/main.tf
+++ aws-basic-tutorial/main.tf
@@ -11,7 +11,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "redis-1" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+  key_name      = "newkey"
+}
+
+resource "aws_instance" "redis-2" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+  key_name      = "newkey"
+}
+
+resource "aws_instance" "redis-3" {
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+  key_name      = "newkey"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "newkey"
+  public_key = file("terraform_ec2_key.pub")
 }
